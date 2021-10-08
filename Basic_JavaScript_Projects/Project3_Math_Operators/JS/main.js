92825245435356103bb5e23507b71634c506dfec
robg245 <robert.guerrero0010@gmail.com>
2021-10-07 18:18:28 -0700

    added notes to main JS Math operators

--- Basic_JavaScript_Projects/Project3_Math_Operators/JS/main.js
+++ Basic_JavaScript_Projects/Project3_Math_Operators/JS/main.js
@@ -1,48 +1,48 @@
 var B = 42 ;
 B--;
-document.write(B);
+document.write(B);  //decrement of 42
 
 
-function My_third_Function () {
-    var operator = "2+2";
-    operator += "=4";
-    document.getElementById("operator").innerHTML = operator;
+function My_third_Function () {     //Function using the "+" operator
+    var operator = "2+2";           //variable of 2+2
+    operator += "=4";               //concatenated =4
+    document.getElementById("operator").innerHTML = operator;   //Displays function 
 }
-function subtraction () {
-    var subtraction = 8-1;
-    document.getElementById("sub").innerHTML = "8-1 = " + subtraction;
-}
-
-function multiplication () {
-    var multiply = 6 * 4 ;
-    document.getElementById("multi").innerHTML = "6 x 4 = " + multiply; 
+function subtraction () {           //subtraction function
+    var subtraction = 8-1;          //function subtracts 8-1
+    document.getElementById("sub").innerHTML = "8-1 = " + subtraction;  //displays subtraction function
 }
 
-function division () {
-    var div = 49 / 7;
-    document.getElementById("divide").innerHTML = "49 / 7 =" + div;
+function multiplication () {        //multiplaction function
+    var multiply = 6 * 4 ;          //variables of 6 *4 used for fucntion
+    document.getElementById("multi").innerHTML = "6 x 4 = " + multiply;     //displays function multiplication
 }
 
-function more () {
-    var math = (4+4) * 12 / 4 - 2;
-    document.getElementById("adv").innerHTML = "4 plus 4, multiplied by 12, divided in 4 and then subtracted by 5 equals " + math;
+function division () {      //division function
+    var div = 49 / 7;       // var div uses 49 / 7 for the function
+    document.getElementById("divide").innerHTML = "49 / 7 =" + div;     //displays division function
 }
 
-function modulus_Operator () {
-    var module = 30 % 4;
-    document.getElementById("mod").innerHTML = "When you divide 30 by 4 you have a remainder of: " + module;
+function more () {              // more function
+    var math = (4+4) * 12 / 4 - 2;  // variable math and the operators / numbers used
+    document.getElementById("adv").innerHTML = "4 plus 4, multiplied by 12, divided in 4 and then subtracted by 5 equals " + math;  //displays the result
 }
 
-function negative () {
-    var x = "6"
-    document.getElementById("neg").innerHTML = -x;
+function modulus_Operator () {      //modulus function
+    var module = 30 % 4;            //variable module uses 30 % 4
+    document.getElementById("mod").innerHTML = "When you divide 30 by 4 you have a remainder of: " + module;    //displays the remainder
 }
 
-var A =10;
-A++;
-document.write(A) ;
+function negative () {          //negative function
+    var x = "6"                 //value of variable x is 6
+    document.getElementById("neg").innerHTML = -x;      //displays the value of -x
+}
+
+var A =10;      //increment function
+A++;            //variable A will be incremented
+document.write(A) ;     //displays incremented value of A
 
 
-window.alert(Math.random() * 25)
+window.alert(Math.random() * 25)    //displays a pop up window of a random value between 0 and 25
 
-document.write(Math.round(3.6)) ;
+document.write(Math.round(3.6)) ;   //displays the rounded value of 3.6
